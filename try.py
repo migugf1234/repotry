98c0816fe44347ac486c6c198627d046e920060c
admin <-->
2022-07-06 09:38:58 +0000

    cambio en el nombre del df

--- try.py
+++ try.py
@@ -3,5 +3,5 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
 import os
 spark = SparkSession.builder.appName("hdfs_test").getOrCreate()
-df2 = spark.read.csv("/opt/ibm/spark/examples/src/main/resources/people.csv")
-df2.show()
+df = spark.read.csv("/opt/ibm/spark/examples/src/main/resources/people.csv")
+df.show()
