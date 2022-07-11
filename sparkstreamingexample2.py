22a0d607411a4bf2b9bac6e309e23dafd5905dad
migugf1234 <100429860+migugf1234@users.noreply.github.com>
2022-07-11 09:22:41 +0200

    Add files via upload

---
+++ sparkstreamingexample2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import*
+from pyspark.sql.types import*
+import time
+
+#create spark session
+spark = SparkSession.builder.getOrCreate()
+
+# Connect to kafka server and read data stream
+df = spark \
+.readStream \
+.format("kafka") \
+.option("kafka.bootstrap.servers", "broker-2-xz4dr8d1l6m6lq49.kafka.svc07.us-south.eventstreams.cloud.ibm.com:9093") \
+.option("kafka.sasl.jaas.config","org.apache.kafka.common.security.plain.PlainLoginModule required username='token' password='bivGCpoxmyPJTIgNB8vV3zBNP9x8K8lRyr8wFyiyFNrP';") \
+.option("kafka.security.protocol", "SASL_SSL") \
+.option("kafka.sasl.mechanism", "PLAIN") \
+.option("kafka.ssl.protocol", "TLSv1.2") \
+.option("kafka.ssl.enabled.protocols", "TLSv1.2") \
+.option("kafka.ssl.endpoint.identification.algorithm", "HTTPS") \
+.option("subscribe", "kafka-java-console-sample-topic") \
+.load() \
+.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+df.printSchema()
+
+# Write the input data to memory
+query = df.writeStream.outputMode("append").format("memory").queryName("testk2s").option("partition.assignment.strategy", "range").start()
+
+query.awaitTermination(3000000)
+
+query.stop()
+
+query.status
+
+# Query data
+test_result=spark.sql("select * from testk2s")
+test_result.show(5)
+
+spark.sql("select count(*) from testk2s").show()
+test_result_small = spark.sql("select * from testk2s limit 5")
+test_result_small.show()
