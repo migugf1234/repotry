ad1400279c4d7095035242a82c088765e11c519c
Miguel Ángel González <miguelangel.gonzalez@ibm.com>
2023-05-26 16:01:55 +0200

    en castellano

--- try.py
+++ try.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         .getOrCreate()
 
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
+    n = 1000000 * partitions
 
     def f(_: int) -> float:
         x = random() * 2 - 1
@@ -23,6 +23,6 @@ if __name__ == "__main__":
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    print("Pi es aproximadamente %f" % (4.0 * count / n))
 
     spark.stop()
